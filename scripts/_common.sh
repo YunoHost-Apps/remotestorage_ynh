46fbc447c8d854200eab1143f462fc837ad2348b
ericgaspar <junk.eg@free.fr>
2021-07-12 10:12:21 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@ YNH_PHP_VERSION="7.3"
 
 YNH_COMPOSER_VERSION="2.0.13"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-spdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl"
+extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl"
 
 pkg_dependencies="libapache2-mod-xsendfile"
 
