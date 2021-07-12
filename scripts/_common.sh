d7f4d567e78d7daa2f0931b111a8fdc4ee620930
ericgaspar <junk.eg@free.fr>
2021-07-12 10:30:44 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,8 +10,6 @@ YNH_COMPOSER_VERSION="2.0.13"
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-pdo php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-cli"
 
-pkg_dependencies="libapache2-mod-xsendfile"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
